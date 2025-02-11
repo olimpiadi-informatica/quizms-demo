b10c53bfcb5c80b35ba1d68c1158027fa58c8c14
Francesco Lugli <francilu26@gmail.com>
2025-02-11 18:25:41 +0100

    [s-4] all but statement

--- src/contest/s-4-percorso-incrementale/visualizer.jsx
+++ src/contest/s-4-percorso-incrementale/visualizer.jsx
@@ -1,82 +1,66 @@
 "use client";
 
+import bunny from "./asy/bunny.asy?w=66";
+import { range } from "lodash-es";
+import { Canvas, Rectangle, Sprite, Variables } from "~/utils/visualizer";
+
 export default function Visualizer({ variables, state }) {
-  return <></>;
-  //const stairs = [stairs1, stairs2, stairs3][state.M <= 6 ? 0 : state.M <= 14 ? 1 : 2];
-  //const bunnies = [bunny_left, bunny_right];
-  //const hf = 2;
-  //const wf = 3;
-  //
-  //const colors = [
-  //  "#4093b0",
-  //  "#bf4eb4",
-  //  "#44c966",
-  //  "#4473c9",
-  //  "#c2a948",
-  //  "#7468c5",
-  //  "#9c5ebd",
-  //  "#7cc258",
-  //  "#a3b651",
-  //  "#40af91",
-  //  "#4473c9",
-  //  "#c2a948",
-  //  "#7468c5",
-  //  "#9c5ebd",
-  //  "#7cc258",
-  //  "#a3b651",
-  //];
-  //
-  //let bsplit = 0;
-  //for (let i = 0; i < state.pos[1]; ++i) {
-  //  if (state.blocchi[i][0] === 1) bsplit += 1;
-  //}
-  //
-  //let bx = 3 - bsplit * (wf + 1);
-  //return (
-  //  <>
-  //    <Canvas gravity="bottom" scale={10}>
-  //      <Rectangle color="#eeeeee" height={state.S * hf} width={wf + 1} x={7} y={0} />
-  //      <Rectangle color="#eeeeee" height={state.D * hf} width={wf + 1} x={23} y={0} />
-  //      <Sprite src={stairs} alt="Scale" x={13.1} y={0} />
-  //      <Rectangle
-  //        color="#954520"
-  //        height={1}
-  //        width={(wf + 1) * state.M * 2 + wf + 17}
-  //        x={7 - (wf + 1) * state.M}
-  //        y={state.pos[3] * hf}
-  //      />
-  //      <Sprite src={bunnies[state.orient]} alt="Bunny" x={14} y={state.pos[3] * hf} follow />
-  //      {range(state.M).map((i) => {
-  //        let x = state.blocchi[i][0] * 8 + 7.5;
-  //        let y = state.blocchi[i][1] * hf;
-  //        if (state.blocchi[i][0] === 1) {
-  //          if (i >= state.pos[1] && bx < 23) bx = 23.5;
-  //          bx += wf + 1;
-  //          x = bx;
-  //          y = state.pos[3] * hf + 0.5;
-  //        }
-  //        return (
-  //          <Rectangle
-  //            key={i}
-  //            color={colors[i % colors.length]}
-  //            height={(i + 1) * hf}
-  //            width={wf}
-  //            x={x}
-  //            y={y}
-  //            className="pl-0.5 leading-4">
-  //            {i + 1}
-  //          </Rectangle>
-  //        );
-  //      })}
-  //    </Canvas>
-  //    <Variables
-  //      variables={{
-  //        sinistra: `${state.pos[0]}/${state.S}`,
-  //        destra: `${state.pos[2]}/${state.D}`,
-  //        blocchetti: `${state.rimasti}/${state.M}`,
-  //        ...variables,
-  //      }}
-  //    />
-  //  </>
-  //);
+  const cell_side = 20;
+  const cell_padding = 1;
+  const scale = 10;
+  const font_height = 4;
+  return <>
+    <Canvas gravity="bottom" scale={scale}>
+      {
+        range(state.N).map((i) =>
+            <Rectangle
+              color="transparent"
+              width={cell_side}
+              height={cell_side}
+              key={"rect" + i}
+              x={cell_side * i}
+              y={font_height*3+1}
+            >
+            <Rectangle
+              color={{W: "white", B: "black"}[state.cols[i]]}
+              width={cell_side-2*cell_padding}
+              height={cell_side-2*cell_padding}
+              x={cell_padding}
+              y={cell_padding}
+            />
+          </Rectangle>
+        )
+      }
+      <Sprite src={bunny} alt="Bunny" x={cell_side*(state.pos+0.5) - 3.5} y={font_height*3+1} follow />
+      <Rectangle style={{
+          overflow: "hidden",
+          padding: "5px",
+          textAlign: "center",
+          "border-radius": "10px",
+      }} x ={cell_side*(state.pos)} y={0} height={font_height*3} width={cell_side}>
+        {range(state.M).map((i) => <Rectangle key={`instr-${i}`} 
+          style={{
+            border: "None",
+            fontSize: scale*font_height,
+            textAlign: "center",
+            fontSize: `${scale*font_height}px`,
+            lineHeight: `${scale*font_height}px`,
+            fontWeight: i == state.i ? "bold":"normal",
+          }}
+          y={(-i+state.i+2)*font_height}
+          width={cell_side}
+          >
+            {{"S":"SALTA", "A":"AVANZA"}[state.instr[i]]}
+        </Rectangle>)}
+      </Rectangle>
+    </Canvas>
+    <Variables variables={{
+      ...variables,
+      "numero di caselle": state.N,
+      "numero di istruzioni": state.M,
+      posizione: state.pos + 1,
+      "istruzione n°": state.i + 1,
+      "istruzioni": state.instr,
+    }} />
+  </>;
 }
