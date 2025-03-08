91696dff2e5e8381a721de0ae6d6d4edbc903693
Francesco Lugli <francilu26@gmail.com>
2025-03-08 14:43:18 +0100

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 import olinfoPresets from "@olinfo/tailwind";
+import path from "node:path";
+
+const quizms = path.dirname(require.resolve("@olinfo/quizms/css"));
+const quizmsMdx = path.dirname(require.resolve("@olinfo/quizms-mdx/css"));
+const reactComponents = path.dirname(require.resolve("@olinfo/react-components"));
 
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [olinfoPresets],
   content: [
     "./src/**/*.{ts,tsx,js,jsx,html}",
-    "./node_modules/@olinfo/quizms/dist/**/*.js",
-    "./node_modules/@olinfo/quizms-mdx/dist/src/**/*.js",
-    "./node_modules/@olinfo/quizms-mdx/dist/*.js",
-    "./node_modules/@olinfo/quizms/node_modules/@olinfo/react-components/dist/**/*.js",
-    "./node_modules/@olinfo/react-components/dist/**/*.js",
+    path.join(quizms, "**/*.js"),
+    path.join(quizmsMdx, "**/*.js"),
+    path.join(reactComponents, "**/*.js"),
   ],
 };
